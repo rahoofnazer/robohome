815421eabbb0a2657dd53b5566f58056b05b1c63
rahoofnazer <rahoofnazer@gmail.com>
2021-06-19 14:47:45 +0530

    bug fixed

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -7,30 +7,28 @@ var collection = require('../config/collection');
 const user_helpers = require('../helpers/user_helpers');
 
 
-
+updateduser = {}
 const generatePdf = async (req, res, next) => {
+       
         const html = fs.readFileSync(path.join(__dirname, '../views/user/template.html'), 'utf-8');
         const filename = 'robohome_certificate_' + Math.random() + '.pdf';
         const user = req.session.user
-        user_helpers.getUser(user).then((response)=>{
+        await user_helpers.getUser(user).then((response)=>{
             console.log('response is');
             console.log(response);
             updateduser= response
-
-            console.log('updated user is printed')
-            console.log(updateduser.user)
-           
+            console.log(updateduser)
           })
 
-    
-        const document = {
+         const document = {
             html: html,
             data: {
-                user: updateduser.user
+                user:  updateduser.user
             },
          
             path: './docs/' + filename
         }
+        
         pdf.create(document, options)
             .then(res => {
                 console.log(res);
