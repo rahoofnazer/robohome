3af500ecc18f25e733dc0863fc80092813cec289
rahoofnazer <rahoofnazer@gmail.com>
2021-06-19 15:11:56 +0530

    bug 2 fixed

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -35,7 +35,7 @@ const generatePdf = async (req, res, next) => {
             }).catch(error => {
                 console.log(error);
             });
-            const filepath = 'http://localhost:3000/docs/' + filename;
+            const filepath = 'http://18.117.84.224/3000/docs/' + filename;
 
             res.render('download', {
                 path: filepath
