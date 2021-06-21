3ac76dd8485ec0c020e5cea63cd7ff4aa96511fc
rahoofnazer <rahoofnazer@gmail.com>
2021-06-21 12:00:20 +0530

    homepage added

--- routes/index.js
+++ routes/index.js
@@ -9,23 +9,49 @@ const session = require('express-session');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect  ('/assessment-test');
+  res.render('index');
 });
 
+router.get('/index', function(req, res, next) {
+  res.redirect('/');
+});
 
-router.get('/assessment-test', function(req, res) {
+router.get('/assessment_test', function(req, res) {
   res.render('assesment-test');
 });
 
+router.get('/book_a_free_session', function(req, res) {
+  res.render('book_a_free_session');
+});
 
-router.post('/quiz', function(req, res, next) { 
-    // req.body.class = req.session.user.name
-    res.redirect('/')
+router.get('/download_brochure', function(req, res) {
+  res.render('download_form');
+});
 
-    const obj = JSON.parse(JSON.stringify(req.body)); 
-    console.log(obj)
-    console.log(req.body)
+router.get('/know_more', function(req, res) {
+  res.render('know_more');
+});
+
+router.get('/ore_best_innovation_challenge', function(req, res) {
+  res.render('ore_best_innovation_challenge');
+});
+
+router.get('/privacypolicy', function(req, res) {
+  res.render('privacypolicy');
+});
+
+router.get('/termsandconditions', function(req, res) {
+  res.render('termsandconditions');
+});
+
+// router.post('/quiz', function(req, res, next) { 
+//     // req.body.class = req.session.user.name
+//     res.redirect('/')
+
+//     const obj = JSON.parse(JSON.stringify(req.body)); 
+//     console.log(obj)
+//     console.log(req.body)
     
-  });
+//   });
   
 module.exports = router;
